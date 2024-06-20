a5747c86fddf4f15d8f4dd5696a7160f46d1d11e
Ivyluoling <ivyluoling@gmail.com>
2024-06-20 15:48:11 +0930

    learned template, dynamic and stayic content in ejs

---
+++ startingproject/app.js
@@ -0,0 +1,66 @@
+const fs = require("fs");
+const path = require("path");
+
+const express = require("express");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static("public")); //check any static files can be found in public folder
+
+app.use(express.urlencoded({ extended: false }));
+
+// without ejs, we need to locate each index html file, and send htmlfile
+// app.get("/", function (req, res) {
+//     const htmlFilePath = path.join(__dirname, "views", "index.html");
+//     res.sendFile(htmlFilePath);
+// });
+
+// with EJS, we will set the html folder to a template folder first
+// use render to change index.ejs to a template
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.get("/restaurants", function (req, res) {
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  res.render("restaurants", {
+    numberOfRestaurants: storedRestaurants.length,
+    restaurants: storedRestaurants,
+  });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+app.get("/recommend", function (req, res) {
+  res.render("recommend");
+});
+
+app.post("/recommend", function (req, res) {
+  //get the input data and stored in json file
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  storedRestaurants.push(restaurant);
+
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  res.redirect("/confirm");
+});
+
+app.get("/confirm", function (req, res) {
+  res.render("confirm");
+});
+
+app.listen(3000);
